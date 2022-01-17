a0ea4fba053a29c75309978f59c205c6b176d8c5
Kim-Gyuri <gyuring0203@gmail.com>
2022-01-18 03:01:09 +0900

    프로젝트 생성 / css추가

---
+++ src/main/java/testim/httpupload/HttpuploadApplication.java
@@ -0,0 +1,13 @@
+package testim.httpupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpuploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/testim/httpupload/HttpuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package testim.httpupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpuploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
